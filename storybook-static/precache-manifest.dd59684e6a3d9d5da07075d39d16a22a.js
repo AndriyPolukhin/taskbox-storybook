eb497aa9f56f709dd0907b6707f17259fece798d
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-09-07 19:43:34 +0300

    Taskbox UI

---
+++ storybook-static/precache-manifest.dd59684e6a3d9d5da07075d39d16a22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ae67eb3d608381bc9659b90836365a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3b6d19447d2b55e85f1c.bundle.js"
+  },
+  {
+    "url": "runtime~main.3b6d19447d2b55e85f1c.bundle.js"
+  },
+  {
+    "revision": "32ab5d27d779c9c24487",
+    "url": "static/css/main.433816b9.chunk.css"
+  },
+  {
+    "url": "vendors~main.3b6d19447d2b55e85f1c.bundle.js"
+  },
+  {
+    "url": "vendors~main.3b6d19447d2b55e85f1c.bundle.js.LICENSE.txt"
+  }
+]);
